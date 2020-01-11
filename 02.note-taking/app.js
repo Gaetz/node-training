cdda0d2c0b905d1f89fc47536406c64a2793133f
gaetz <gaetz@rufflerim.com>
2020-01-11 13:31:57 +0100

    Using modules

--- 02.note-taking/app.js
+++ 02.note-taking/app.js
@@ -1,13 +1,28 @@
-const validator = require('validator');
-const add = require('./utils');
+// Personal modules
+const { name, add } = require('./utils');
 const getNotes = require('./notes.js');
 
+// Npm local modules
+const validator = require('validator');
+const chalk = require('chalk');
 
+// User personal modules
 const sum = add(9, 15);
 console.log(sum);
 
 const msg = getNotes();
 console.log(msg);
 
+// Use npm modules
+// - String validation with validator
 console.log(validator.isEmail('gaetz@rufflerim.com'));
 console.log(validator.isURL('http:/rufflerm.com'));
+
+// - Console color with chalk
+console.log(chalk.blue.bgGreen.bold('success'));
+console.log(chalk.green.inverse.bold('other success'));
+console.log(chalk.red('fail'));
+
+// Install nodemon for auto update when you save your program
+// (sudo) npm install -g nodemon
+
