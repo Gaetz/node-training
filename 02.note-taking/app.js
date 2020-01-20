f9ce8f0ca2ea76120c7e285873222596d9a49786
gaetz <gaetz@rufflerim.com>
2020-01-20 23:51:44 +0100

    Switch to yard and add yargs module

--- 02.note-taking/app.js
+++ 02.note-taking/app.js
@@ -1,6 +1,7 @@
 const yargs = require('yargs');
 const validator = require('validator');
 const chalk = require('chalk');
+const fs = require('fs');
 
 /* AT FIRST
 
@@ -37,16 +38,21 @@ yargs.command('list', 'List notes', {}, () => {
 
 console.log(yargs.argv)
 
+// Run: 'node app.js --help' to see the documentation
+// Run: 'node app.js add --title="Things to buy"' to see it in action
 
  */
 
+/* 2. Using params
 
 yargs.command('add', 'Add a note',
     {
-        title: { describe: 'Note title', demandOption: true, type: 'string' }
+        title: { describe: 'Note title', demandOption: true, type: 'string' },
+        body: { describe: 'Note body', demandOption: true, type:'string' }
     },
     function(argv) {
         console.log("Title: ", argv.title);
+        console.log("Body: ", argv.body);
     });
 yargs.command('remove', 'Remove a note', {}, () => {
     console.log("Remove a note");
@@ -60,5 +66,69 @@ yargs.command('list', 'List notes', {}, () => {
 
 yargs.parse();
 
-// Run: 'node app.js --help' to see the documentation
-// Run: 'node app.js add --title="Things to buy"' to see it in action
+// Run: 'node app.js add --title="Things to buy" --body="ice cream, spinach"' to see it in action
+
+*/
+
+
+/* Using Json */
+
+// Javascript object
+const book = {
+    title: 'Le discours de la méthode',
+    author: 'René Descartes'
+}
+
+// Converted to json
+const bookData = JSON.stringify(book);
+console.log(bookData);
+//console.log(bookData.title) won't work;
+
+// Json to object
+const parsedData = JSON.parse(bookData);
+console.log(parsedData.title); // Work this time
+
+// Store into a file
+fs.writeFileSync('book.json', bookData);
+
+// Read the file
+const dataBuffer = fs.readFileSync('book.json');
+const dataJSON = dataBuffer.toString();
+const loadedBook = JSON.parse(dataJSON);
+console.log(loadedBook.author);
+
+
+/* 3. */
+const notes = require('./notes');
+
+yargs.command('add', 'Add a note',
+    {
+        title: { describe: 'Note title', demandOption: true, type: 'string' },
+        body: { describe: 'Note body', demandOption: true, type:'string' }
+    },
+    function(argv) {
+        notes.addNote(argv.title, argv.body);
+    });
+
+/*
+* Exercice: create the remove note function which takes a title as a parameter
+* Add colors in the console with chalk
+* */
+
+
+yargs.command('remove', 'Remove a note', {title: { describe: 'Note title', demandOption: true, type: 'string' }}, (argv) => {
+    notes.removeNote(argv.title);
+});
+
+/* Exercice: now list notes */
+
+yargs.command('read', 'Read a note', {title: { describe: 'Note title', demandOption: true, type: 'string' }}, (argv) => {
+    notes.readNote(argv.title);
+});
+yargs.command('list', 'List notes', {}, () => {
+    notes.listNotes();
+});
+
+yargs.parse();
+
+// Run: 'node app.js add --title="Things to buy" --body="ice cream, spinach"' to see it in action
--- 02.note-taking/notes.js
+++ 02.note-taking/notes.js
@@ -1,5 +1,86 @@
+const fs = require('fs');
+
 function getNotes() {
     return 'Your notes...'
 }
 
-module.exports = getNotes;
+function addNote(title, body) {
+    const notes = loadNotes();
+
+    /* Option to avoid multiple same titles */
+    const duplicateNotes = notes.filter((note) => {
+        return note.title === title;
+    });
+    if (duplicateNotes.length == 0) {
+        notes.push({title: title, body: body});
+        saveNotes(notes);
+        console.log("New note added");
+    } else {
+        console.log("A note with this title already exists");
+    }
+
+    /*
+    notes.push({ title: title, body: body });
+    saveNotes(notes);
+     */
+}
+
+function loadNotes() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch(e) {
+        return [];
+    }
+}
+
+function saveNotes(notes) {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+
+function removeNote(title) {
+    const notes = loadNotes();
+
+    const notesToKeep = notes.filter((note) => {
+        return note.title !== title;
+    });
+
+    if(notesToKeep.length < notes.length) {
+        console.log('Note removed');
+    }
+
+    saveNotes(notesToKeep);
+
+}
+
+function listNotes() {
+    const notes = loadNotes();
+    console.log("NOTES:");
+    notes.forEach((note) => {
+       console.log("Title:" + note.title);
+    });
+}
+
+function readNote(title) {
+    const notes = loadNotes();
+    const note = notes.find((note) => {
+        return note.title === title;
+    });
+    if(note) {
+        console.log("Title:" + note.title);
+        console.log("Body:" + note.body);
+    } else {
+        console.log("This note does not exists");
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+}
