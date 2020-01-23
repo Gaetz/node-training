e70962b78cb3dd0782c0b32f7fc3f65e764aa19d
gaetz <gaetz@rufflerim.com>
2020-01-23 08:06:05 +0100

    Async functions working together

--- 03.weather-app/app.js
+++ 03.weather-app/app.js
@@ -80,25 +80,42 @@ function geocode(city, callback) {
 
     request( {url: url, json: true} , (error, response) => {
         if(error) {
-            callback('Unable to connect to weather API.');
+            callback('Unable to connect to geocode API.');
         } else if(response.body.features.length === 0) {
             callback('Unable to find location');
         } else {
             const latitude = response.body.features[0].center[1];
             const longitude = response.body.features[0].center[0];
-            const data = { latitude:latitude, longitude:longitude };
+            const data = { latitude, longitude }; // Use shortcut to fill fields
             callback(error, data);
         }
     });
 }
 
-function displayWeather(data) {
+function displayWeather(data, callback) {
     const url = 'https://api.darksky.net/forecast/ad95e11dbf8fe6961a05425dea338c24/' + data.latitude + ',' + data.longitude + '?units=si&lang=fr';
+
     request({url: url, json: true}, (error, response) => {
-        //console.log(response.body.currently);
-        const body = response.body;
-        console.log(body.daily.data[0].summary + ' Il fait actuellement ' + body.currently.temperature + ' degrés et le risque de pluie est de ' + body.currently.precipProbability * 100 + '%.');
+        if (error) {
+            callback('Unable to connect to weather API.');
+        } else if (response.body.error) {
+            callback('Unable to find location\'s weather');
+        } else {
+            const { temperature, precipProbability } = response.body.currently; // Use destructuring to create the two variables
+            callback(undefined, response.body.daily.data[0].summary + ' Il fait actuellement ' + temperature + ' degrés et le risque de pluie est de ' + precipProbability * 100 + '%.');
+        }
     });
 }
 
-geocode('Montpellier', (error, data) => displayWeather(data));
+geocode('Montpellier', (error, data) => {
+    if(error) {
+        return console.log(error);
+    }
+
+    displayWeather(data, (error, data) => {
+        if(error) {
+            return console.log(error);
+        }
+        console.log(data);
+    })
+});
