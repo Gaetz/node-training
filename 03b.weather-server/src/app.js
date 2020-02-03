86523a2f0c3a844faf6019b8980e512edebbac79
Gaetz <gaetan.blaisecazalet@gmail.com>
2020-02-03 20:12:15 +0100

    Webserver now hit weather API on a route

--- 03b.weather-server/src/app.js
+++ 03b.weather-server/src/app.js
@@ -5,16 +5,31 @@ const weatherlib = require('./weather');
 
 const app = express();
 
-
 const publicStaticDir = path.join(__dirname, '../public');
 app.use(express.static(publicStaticDir));
 
-
 // For default path
+
+/* 
+// 1. Simple text
 app.get('', (req, res) => {     // req: request data, res: response
     res.send('Hello express');
 });
 
+// 2. HTML
+app.get('', (req, res) => {     // req: request data, res: response
+    res.send('<h1>Weather</h1>');
+});
+*/
+// 3. JSON
+app.get('', (req, res) => {     // req: request data, res: response
+    res.send({
+        name: 'Gaëtan',
+        age: 32
+    });
+});
+
+/*
 app.get('/help', (req, res) => {
     res.sendFile(publicStaticDir + '/help.html');
 });
@@ -26,8 +41,9 @@ app.get('/about', (req, res) => {
         age: 33
     });
 });
+*/
 
-
+/*
 app.get('/weather', (req, res) => {
     const weather = weatherlib.weather('Montpellier', (error, data) =>{
         console.log(data);
@@ -38,6 +54,85 @@ app.get('/weather', (req, res) => {
 
 app.get('*', (req, res) => {
     res.status(404).send('This page does not exists');
+});
+
+// Exercice: return a weather json object with a forcast and a location
+*/
+/*
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'Sunny',
+        location: 'Montpellier'
+    });
+});
+*/
+// Now we will serve files of a 'public' folder we create in our server folder
+
+// We can use those two variable to know were we are with node
+console.log(__dirname);
+console.log(__filename);
+
+// We can also use the path module
+//const path = require('path');
+console.log(path.join(__dirname, '../public'));
+
+// Now go up and configure the server to use the public folder
+// We create about and help html files in public, removing the node handler 
+// We create a styles.css file in the css folder.
+// We add :         <link rel="stylesheet" href="css/styles.css" /> in the index file, and to the other files
+// Then create a js folder in the public folder, then a app.js file in this folder
+// We add :        <script src="js/app.js"></script> in the index filde
+// Create a img folder in the public folder and put an image inside
+// We add :         <img src="img/godot.png" />
+
+// Now we will add a query string to get the weather from a city. 
+// Request in navigator will be: http://localhost:3000/weather?city=Montpellier
+
+/*
+app.get('/weather', (req, res) => {
+    if(!req.query.city) {
+        return res.send({
+            error: 'You must provide a city'
+        });
+    }
+
+    res.send({
+        forecast: 'Sunny',
+        location: req.query.city
+    });
+});
+*/
+
+// Now create a utils.js file that contains the geocode and the displayWeather functions.
+// Exercice: Use this route to send back in a json request the weather forecast, using our new utils.
+// Dont forget to install request.
+
+const geocode = require('./utils/geocode');
+const displayWeather = require('./utils/displayWeather');
+
+app.get('/weather', (req, res) => {
+    if(!req.query.city) {
+        return res.send({
+            error: 'You must provide a city'
+        });
+    }
+
+    geocode(req.query.city, (error, data) => {
+        if(error) {
+            return res.send({ error });
+        }
+    
+        displayWeather(data, (error, resultData) => {
+            if(error) {
+                return res.send({ error });
+            }
+
+            res.send({
+                forecast: resultData,
+                location: req.query.city
+            });
+        })
+    });
 });
 
 // Starts the server on port 3000
---
+++ 03b.weather-server/src/utils.js
@@ -0,0 +1,38 @@
+const request = require('request');
+
+function geocode(city, callback) {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(city)+'.json?&language=fr&access_token=' + mapboxToken;
+
+    request( {url: url, json: true} , (error, response) => {
+        if(error) {
+            callback('Unable to connect to geocode API.');
+        } else if(response.body.features.length === 0) {
+            callback('Unable to find location');
+        } else {
+            const latitude = response.body.features[0].center[1];
+            const longitude = response.body.features[0].center[0];
+            const data = { latitude, longitude }; // Use shortcut to fill fields
+            callback(error, data);
+        }
+    });
+}
+
+function displayWeather(data, callback) {
+    const url = 'https://api.darksky.net/forecast/ad95e11dbf8fe6961a05425dea338c24/' + data.latitude + ',' + data.longitude + '?units=si&lang=fr';
+
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather API.');
+        } else if (response.body.error) {
+            callback('Unable to find location\'s weather');
+        } else {
+            const { temperature, precipProbability } = response.body.currently; // Use destructuring to create the two variables
+            callback(undefined, response.body.daily.data[0].summary + ' Il fait actuellement ' + temperature + ' degrés et le risque de pluie est de ' + precipProbability * 100 + '%.');
+        }
+    });
+}
+
+module.exports = {
+    geocode: geocode,
+    displayWeather: displayWeather
+}
