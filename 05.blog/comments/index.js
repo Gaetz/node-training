873f34222d9fc3734c85344a7b6cd5d2f7cb00b8
Gaetz <gaetz@rufflerim.com>
2020-06-29 08:42:43 +0200

    Microservice blog

---
+++ 05.blog/comments/index.js
@@ -0,0 +1,29 @@
+/* 2. Create a comment application for our posts */
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const commentsByPostIds = {}
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostIds[req.params.id] || [])
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex')
+    const { content } = req.body
+
+    const comments = commentsByPostIds[req.params.id] || []
+    comments.push({ id: commentId, content })
+    commentsByPostIds[req.params.id] = comments
+    res.status(201).send(comments)
+})
+
+app.listen(4001, () => {
+    console.log('Comments server listening on 4001')
+})
---
+++ 05.blog/posts/index.js
@@ -0,0 +1,28 @@
+/* 1. Create a post application */
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body
+
+    posts[id] = { id, title }
+
+    res.status(201).send(posts[id])
+})
+
+app.listen(4000, () => {
+    console.log('Posts server listening on 4000')
+})
