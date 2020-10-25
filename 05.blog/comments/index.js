75b87dbf3b4e06614f3ea4afc72c1cfc65301976
Gaetz <gblaisec@artfx.fr>
2020-10-25 18:36:37 +0100

    Sending and receiving events

--- 05.blog/comments/index.js
+++ 05.blog/comments/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -16,6 +17,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostIds[req.params.id] || [])
 })
 
+/*
 app.post('/posts/:id/comments', (req, res) => {
     const commentId = randomBytes(4).toString('hex')
     const { content } = req.body
@@ -24,6 +26,29 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({ id: commentId, content })
     commentsByPostIds[req.params.id] = comments
     res.status(201).send(comments)
+})
+*/
+
+app.post('/posts/:id/comments', async (req, res) => {
+    const commentId = randomBytes(4).toString('hex')
+    const { content } = req.body
+
+    const comments = commentsByPostIds[req.params.id] || []
+    comments.push({ id: commentId, content })
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: { id: commentId, content, postId: req.params.id }
+        }
+    )
+
+    commentsByPostIds[req.params.id] = comments
+    res.status(201).send(comments)
+})
+
+app.post('/events', async (req, res) => {
+    console.log('Received Event:', req.body.type)
+    res.send({})
 })
 
 app.listen(4001, () => {
