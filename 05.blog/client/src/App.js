612b8b7653bdabaeeac7b9793f0f83f3b0efcea0
Gaetz <gblaisec@artfx.fr>
2020-10-25 16:22:53 +0100

    Create post handled with cors

--- 05.blog/client/src/App.js
+++ 05.blog/client/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import PostCreate from './PostCreate'
 
 export default () => {
-    return <div>
+    return <div className="container">
         <h1>Create post</h1>
         <PostCreate/>
     </div>
--- 05.blog/comments/index.js
+++ 05.blog/comments/index.js
@@ -3,10 +3,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 const commentsByPostIds = {}
 
